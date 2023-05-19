cc881ce2e180a58016c08345d444796a40b49ec6
Frederic Brenner <frederic.brenner@tum.de>
2023-05-19 18:29:34 +0200

    add target reach display, fix input file

--- ai/training.py
+++ ai/training.py
@@ -24,16 +24,21 @@ def train(csv_file_name):
     # Set training parameters
     learning_rate = 8e-5
     n_epochs = 300
-    batch_size = 64
-    neuron_size = 128
-    loss = 'mse'
+    batch_size = 256
+    neuron_size = 80
+    # loss = 'mean_squared_error'
+    loss = 'mean_squared_logarithmic_error'
+    # loss = 'mean_absolute_percentage_error'
+    # loss = 'mean_absolute_error'
+    # loss = 'cosine_similarity'
 
     # setup ML model
     model = create_tf_model(model_name, dim_in=[1, 1],
                             dim_out=ml_out.shape[1], nr=neuron_size)
 
     adam = adam_v2.Adam(learning_rate=learning_rate, decay=learning_rate / n_epochs)
-    model.compile(loss=loss, optimizer=adam, metrics=['mean_squared_logarithmic_error'])
+    model.compile(loss=loss, optimizer=adam)
+    # model.compile(loss=loss, optimizer=adam, metrics=['mean_squared_logarithmic_error'])
 
     # Train autoencoder
     training = model.fit(x=ml_in, y=ml_out, epochs=n_epochs,
@@ -72,7 +77,8 @@ def predict(ml_in=None):
     y = model.predict(ml_in, verbose=0)
     # result = np.vstack((ml_in[:, 1], y[:, 0])).T
     result = np.vstack((ml_in[1], y[:, 0])).T
-    print(result)
+    # print(result)
+    return result
     # y_df = pd.DataFrame(y, columns=y_col)
     # y = scaler_y.inverse_transform(y_df)
     # y_df = pd.DataFrame(y, columns=y_col)
@@ -82,15 +88,26 @@ def predict(ml_in=None):
 if __name__ == '__main__':
     # csv_file_name = '../0.51-0.49 #11-111.csv'
     # csv_file_name = '../0.51-0.49 #11-211(only odd numbers).csv'
-    csv_file_name = '../0.51-0.49 #11-3011.csv'
+    # csv_file_name = '../0.51-0.49 #11-3011.csv'
+    # csv_file_name = '../0.51-0.49 #11-6011 (only odd numbers).csv'
+    # csv_file_name = '../0.5105-0.4895 #11-6011 (only odd numbers).csv'
+    csv_file_name = ['../0.51-0.49 #11-6011 (only odd numbers).csv',
+                     '../0.5105-0.4895 #11-6011 (only odd numbers).csv']
 
     # Training
-    train(csv_file_name)
+    # train(csv_file_name)
 
     # Prediction
-    par_in = 0.51
-    pos_in = np.arange(2, 15.01, 0.1)
+    par_in = 0.5105
+    pos_in = np.arange(2, 13.01, 0.1)
     pos_in = np.round(np.exp(pos_in))
     # ml_in = np.vstack([[par_in] * len(pos_in), pos_in]).T
     ml_in = [np.asarray([par_in] * len(pos_in)), pos_in]
-    predict(ml_in)
+    pred = predict(ml_in)
+    threshold = 0.99999
+    target = pred[pred[:, 1] > threshold, 0]
+    if len(target) == 0:
+        print(f"1.0 reached at: not reached")
+        print(pred)
+    else:
+        print(f"1.0 reached at: {target[0]}")
--- parameter_estimation/import_csv.py
+++ parameter_estimation/import_csv.py
@@ -3,12 +3,21 @@ import pandas as pd
 
 
 def import_csv(csv_fn, check_size=True):
-    df = pd.read_csv(csv_fn, sep=',', header=0)
-    if check_size:
-        size_mask = df['size'] % 2 == 1
-        df = df[size_mask]
+    if type(csv_fn) is not list:
+        csv_fn = [csv_fn]
+
+    df_all = None
+    for fn in csv_fn:
+        df = pd.read_csv(fn, sep=',', header=0)
+        if check_size:
+            size_mask = df['size'] % 2 == 1
+            df = df[size_mask]
+        if df_all is None:
+            df_all = df
+        else:
+            df_all = df_all.append(df, ignore_index=True)
     # print(df)
-    return df
+    return df_all
 
 
 if __name__ == '__main__':

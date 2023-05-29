29aefc146523c15512260ccd21d7c4cebaf09b92
Frederic Brenner <frederic.brenner@tum.de>
2023-05-29 23:12:33 +0200

    auto training evaluation

--- ai/training.py
+++ ai/training.py
@@ -18,10 +18,19 @@ sess = tf.compat.v1.Session(config=conf)
 tf.compat.v1.keras.backend.set_session(sess)
 
 
-def train(csv_file_name, learning_rate, neuron_size, batch_size, n_epochs):
+def train(csv_file_name, learning_rate, n_epochs,
+          neuron_size=64, save_model=True, model=None):
     # import data
     csv_data = import_csv(csv_file_name, check_size=False)
     ml_in, ml_out = preprocess_data(csv_data)
+    if len(ml_out) > 500:
+        batch_size = 32
+    elif len(ml_out) > 200:
+        batch_size = 16
+    elif len(ml_out) > 100:
+        batch_size = 8
+    else:
+        batch_size = 4
 
     # Check Cuda compatible GPU
     if not test_gpu_tf():
@@ -39,25 +48,28 @@ def train(csv_file_name, learning_rate, neuron_size, batch_size, n_epochs):
     # loss = 'cosine_similarity'
 
     # setup ML model
-    model = create_tf_model(model_name, dim_in=[1, 1],
-                            dim_out=ml_out.shape[1], nr=neuron_size)
+    if model is None:
+        model = create_tf_model(model_name, dim_in=[1, 1],
+                                dim_out=ml_out.shape[1], nr=neuron_size)
 
-    adam = adam_v2.Adam(learning_rate=learning_rate, decay=learning_rate / n_epochs)
-    model.compile(loss=loss, optimizer=adam)
-    # model.compile(loss=loss, optimizer=adam, metrics=['mean_squared_logarithmic_error'])
+        adam = adam_v2.Adam(learning_rate=learning_rate, decay=learning_rate / n_epochs)
+        model.compile(loss=loss, optimizer=adam)
+        # model.compile(loss=loss, optimizer=adam, metrics=['mean_squared_logarithmic_error'])
 
     # Train autoencoder
     training = model.fit(x=ml_in, y=ml_out, epochs=n_epochs,
                          batch_size=batch_size, shuffle=True, verbose=1)
+    if save_model:
+        # Save model
+        inp = 'y'
+        # inp = input("Save model? (y or n)")
+        if inp.lower() == 'y':
+            save_path = f'ai/dumps/{model_name}.h5'
+            print(f"Saving model at: {save_path}")
+            model.save(save_path)
 
-    # Save model
-    inp = input("Save model? (y or n)")
-    if inp.lower() == 'y':
-        save_path = f'ai/dumps/{model_name}.h5'
-        print(f"Saving model at: {save_path}")
-        model.save(save_path)
-
-    print("\nFinished Training")
+        print("\nFinished Training")
+    return model, training
 
 
 def predict(ml_in=None):
@@ -108,31 +120,54 @@ if __name__ == '__main__':
 
     # 4P
     # csv_file_name = '../0.3, 0.25, 0.25, 0.2 #12-1012 (only multiples of 4).csv'  # unknown (~2.500)
-    # csv_file_name = '../0.26, 0.25, 0.25, 0.24 #12-1001 (only multiples of 4).csv'  # unknown (>45k?)
-    csv_file_name = '../0.27, 0.26, 0.26, 0.21 #12-1012 (only multiples of 4).csv'  # unknown
+    csv_file_name = '../0.26, 0.25, 0.25, 0.24 #12-1001 (only multiples of 4).csv'  # unknown (>45k?)
+    # csv_file_name = '../0.27, 0.26, 0.26, 0.21 #12-1012 (only multiples of 4).csv'  # unknown
 
     # Training
-    lr = 7.75e-5
+    lr = 7.8e-5
     nr = 128
-    batch_size = 8
-    n_epochs = 800
-    train(csv_file_name, lr, nr, batch_size, n_epochs)
+    n_ep = 800
+    n_ep_test = 120
+    accuracy_test = 0.0018
 
-    # Prediction
     par_in = csv_file_name.split('/')[1].split('-')[0]
     if len(par_in) > 10:
         par_in = par_in.split(',')[0]
     par_in = float(par_in)
-    print(par_in)
-    pos_in = np.arange(5, 28.01, 0.02)
-    pos_in = np.round(np.exp(pos_in))
-    # ml_in = np.vstack([[par_in] * len(pos_in), pos_in]).T
-    ml_in = [np.asarray([par_in] * len(pos_in)), pos_in]
-    pred = predict(ml_in)
-    threshold = 0.99999
-    target = pred[pred[:, 1] > threshold, 0]
-    if len(target) == 0:
-        print(f"1.0 reached at: not reached")
-        print(pred)
-    else:
-        print(f"1.0 reached at: {target[0]}")
+
+    result_runs = []
+    loss_runs = []
+    for runs in range(5):
+        for i in range(5):
+            model, training = train(csv_file_name, learning_rate=lr, neuron_size=nr,
+                                    n_epochs=n_ep_test, save_model=False)
+            if training.history['loss'][-1] < accuracy_test:
+                break
+        if i == 4:
+            print(f"Training did not converge: {par_in}")
+        else:
+            print(f"Took {i+1} iterations to converge.")
+            _, training = train(csv_file_name, learning_rate=lr, n_epochs=n_ep,
+                  model=model, save_model=True)
+
+        # Prediction
+        print(par_in)
+        pos_in = np.arange(5, 28.01, 0.02)
+        pos_in = np.round(np.exp(pos_in))
+        ml_in = [np.asarray([par_in] * len(pos_in)), pos_in]
+        pred = predict(ml_in)
+        threshold = 0.99999
+        target = pred[pred[:, 1] > threshold, 0]
+        if len(target) == 0:
+            print(f"1.0 reached at: not reached")
+            print(pred)
+        else:
+            print(f"1.0 reached at: {target[0]} with training loss: {training.history['loss'][-1]}")
+
+        # Add results to list
+        result_runs.append(target[0])
+        loss_runs.append(training.history['loss'][-1])
+
+    print(result_runs)
+    print(loss_runs)
+
